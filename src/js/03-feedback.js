1272275805699f60fad8cf63595a82bd4870c2c2
Logvinov Roman <roman.logvinov88@gmail.com>
2022-07-22 19:46:24 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const currentTimeKey = "feedback-form-state";
+const formInput = {
+    email: "",
+    message: "",
+};
+
+if (localStorage.getItem(currentTimeKey) === null) {
+    localStorage.setItem(currentTimeKey, JSON.stringify(formInput));
+} ;
+
+feedbackForm.elements.email.value = JSON.parse(localStorage.getItem(currentTimeKey)).email;
+feedbackForm.elements.message.value = JSON.parse(localStorage.getItem(currentTimeKey)).message;
+feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+feedbackForm.addEventListener('submit', throttle(onFormInputSubmit, 500));
+
+function onFormInput(e) {
+    formInput.email = feedbackForm.elements.email.value;
+    formInput.message = feedbackForm.elements.message.value;
+
+    localStorage.setItem(currentTimeKey, JSON.stringify(formInput));
+    
+}
+
+function onFormInputSubmit(evt) {
+    evt.preventDefault();
+    localStorage.removeItem(currentTimeKey);
+    feedbackForm.reset();
+    console.log(formInput);
+} 
