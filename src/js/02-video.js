6e3816cfa1bb2b6d0eb5c06cb1e30bb133b24ae0
Logvinov Roman <roman.logvinov88@gmail.com>
2022-07-22 19:21:51 +0300

    player done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
+const currentTimeKey = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.setCurrentTime(localStorage.getItem(currentTimeKey));
+
+player.on('timeupdate', throttle(() => {
+    player.getCurrentTime().then((seconds) => {
+        localStorage.setItem(currentKey, seconds);
+    });
+}, 1000)
+);
